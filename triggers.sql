a60a0c3991f40a760daab7149e1c21cd36daeec7
Thomaz <thomaz2pinheiro@gmail.com>
2022-11-13 17:00:39 -0300

    triggers
    trigger principal feito

--- triggers.sql
+++ triggers.sql
@@ -19,7 +19,7 @@ CREATE OR REPLACE FUNCTION update_turma() RETURNS TRIGGER
 AS
 $BODY$
 BEGIN
-    IF NEW.vagas < NEW.qnt_alunos
+    IF NEW.vagas < NEW.qnt_alunos OR NEW.qnt_alunos < 0
     THEN RAISE EXCEPTION 'limite da turma atingido.';
     ELSE
         RETURN NEW;
@@ -32,3 +32,19 @@ CREATE TRIGGER T_update_turma
 AFTER UPDATE ON turma
 FOR EACH ROW
 EXECUTE PROCEDURE update_turma();
+
+CREATE OR REPLACE FUNCTION rem_aluno() RETURNS TRIGGER 
+AS
+$BODY$
+BEGIN
+        UPDATE turma t
+        SET qnt_alunos = qnt_alunos - 1 where t.id = OLD.id_turma;
+        RETURN NEW;
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER T_REMALUNO
+AFTER DELETE ON aluno_turma_disc
+FOR EACH ROW
+EXECUTE PROCEDURE rem_aluno();
