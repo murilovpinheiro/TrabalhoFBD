e153f0e7e3df4229924e54284e27ae0462dd6808
Thomaz <thomaz2pinheiro@gmail.com>
2022-11-15 22:57:55 -0300

    Create querys_especificas.sql

---
+++ sql/querys_especificas.sql
@@ -0,0 +1,28 @@
+-- 1. Visualizar a média de cada aluno matriculado em uma turma já concluída.
+-- checar no python se a turma entrada eh fechada (false)
+SELECT m.matr_aluno as matricula, m.media, t.id as turma_id, t.disciplina_id
+FROM (SELECT matr_aluno, AVG(valor) as media
+	  FROM notas 
+	  GROUP BY matr_aluno) m, aluno_turma_disc atd, turma t
+WHERE t.id = '1' AND atd.id_disc = t.disciplina_id AND atd.id_turma = t.id AND m.matr_aluno IN -- id mutavel
+(SELECT matr_aluno FROM notas WHERE id_disc = t.disciplina_id); 
+
+-- 2. Verificar todas as turmas de determinado semestre.
+SELECT * from turma
+WHERE semestre = '{1111.2}'; -- semestre mutavel
+
+-- 6. Visualizar as médias de um aluno dada a matrícula. (Similar a um histórico escolar)
+SELECT matr_aluno as matricula, AVG(valor) as media
+FROM notas
+WHERE matr_aluno = '509299' -- matricula mutavel
+GROUP BY matr_aluno;
+
+SELECT * FROM notas;
+
+DELETE FROM notas where id = 4;
+
+insert into notas values(7,7.8,23,1);
+
+SELECT matr_aluno, AVG(valor)
+FROM notas 
+GROUP BY matr_aluno;
